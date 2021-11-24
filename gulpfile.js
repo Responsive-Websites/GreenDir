fb50eec40c01a64aa3e2112fe588afc4bf0dc09a
Danila-hub <KonTaigaBox@mail.ru>
2021-11-24 21:34:59 +0300

    Added builder files and start dirs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 let project_folder = 'dist';
 let source_folder = '#src';
+let fs = require('fs');
 
 let path = {
   build: {
@@ -13,7 +14,7 @@ let path = {
     html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
     css: source_folder + '/scss/style.scss',
     js: source_folder + '/js/script.js',
-    img: source_folder + '/img/**/*.{jpg, png, svg, gif, ico, webp, jpeg}',
+    img: source_folder + '/img/**/*.+(png|jpg|gif|ico|svg|webp)',
     fonts: source_folder + '/fonts/*.ttf',
   },
   watch: {
@@ -36,7 +37,14 @@ let { src, dest } = require('gulp'),
   clean_css = require('gulp-clean-css'),
   rename = require('gulp-rename'),
   uglify = require('gulp-uglify-es').default,
-  imagemin = require('gulp-imagemin');
+  imagemin = require('gulp-imagemin'),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html'),
+  webpcss = require('gulp-webpcss'),
+  svgSprite = require('gulp-svg-sprite'),
+  ttf2woff = require('gulp-ttf2woff'),
+  ttf2woff2 = require('gulp-ttf2woff2'),
+  fonter = require('gulp-fonter');
 
 function browserSync(params) {
   browsersync.init({
@@ -51,6 +59,7 @@ function browserSync(params) {
 function html() {
   return src(path.src.html)
     .pipe(fileinclude())
+    .pipe(webphtml())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream());
 }
@@ -69,6 +78,7 @@ function css() {
         cascade: true,
       })
     )
+    .pipe(webpcss())
     .pipe(dest(path.build.css))
     .pipe(clean_css())
     .pipe(
@@ -97,6 +107,13 @@ function js() {
 function images() {
   return src(path.src.img)
     .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
       imagemin({
         progressive: true,
         svgoPlugins: [{ removeViewBox: false }],
@@ -108,6 +125,68 @@ function images() {
     .pipe(browsersync.stream());
 }
 
+function fonts() {
+  src(path.src.fonts).pipe(ttf2woff()).pipe(dest(path.build.fonts));
+
+  return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
+}
+
+gulp.task('otf2ttf', function () {
+  return src([source_folder + '/fonts/*.otf'])
+    .pipe(
+      fonter({
+        formats: ['ttf'],
+      })
+    )
+    .pipe(dest(source_folder + '/fonts/*'));
+});
+
+gulp.task('svgSprite', function () {
+  return gulp
+    .src([source_folder + '/iconsprite/*.svg'])
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../icons/icons.svg',
+            example: true,
+          },
+        },
+      })
+    )
+    .pipe(dest(path.build.img));
+});
+
+function fontsStyle(params) {
+  let file_content = fs.readFileSync(source_folder + '/scss/fonts.scss');
+  if (file_content == '') {
+    fs.writeFile(source_folder + '/scss/fonts.scss', '', cb);
+    return fs.readdir(path.build.fonts, function (err, items) {
+      if (items) {
+        let c_fontname;
+        for (var i = 0; i < items.length; i++) {
+          let fontname = items[i].split('.');
+          fontname = fontname[0];
+          if (c_fontname != fontname) {
+            fs.appendFile(
+              source_folder + '/scss/fonts.scss',
+              '@include font("' +
+                fontname +
+                '", "' +
+                fontname +
+                '", "400", "normal");\r\n',
+              cb
+            );
+          }
+          c_fontname = fontname;
+        }
+      }
+    });
+  }
+}
+
+function cb() {}
+
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
   gulp.watch([path.watch.css], css);
@@ -118,9 +197,16 @@ function clean(params) {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
+let build = gulp.series(
+  clean,
+  gulp.parallel(js, css, html, images, fonts),
+  fontsStyle
+);
 let watch = gulp.parallel(build, watchFiles, browserSync);
+scss({ outputStyle: 'expanded' }).on('error', scss.logError);
 
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
