63948a5c092e5051d6f75a3b55385399c97cc1cf
Danila-hub <kontaigabox@mail.ru>
2021-12-06 15:43:41 +0000

    [Issue #3] - Added burger menu and styles for it

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,15 +5,25 @@ function testWebP(callback) {
   webP.onload = webP.onerror = function () {
     callback(webP.height == 2);
   };
-  webP.src =
-    'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
   if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    console.log('work');
+    document.querySelector("body").classList.add("webp");
   } else {
-    document.querySelector('body').classList.add('no-webp');
+    document.querySelector("body").classList.add("no-webp");
   }
 });
+
+// burger menu
+
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu) {
+  const menuBody = document.querySelector(".menu__list");
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+  });
+}
